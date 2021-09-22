99883ab1c98570406302715db2ea3c12db818154
marlfoxlegend <30681614+marlfoxlegend@users.noreply.github.com>
2021-09-22 00:42:56 -0400

    change how files are copied

--- Get-WinPhotos.ps1
+++ Get-WinPhotos.ps1
@@ -1,47 +1,47 @@
-function Get-SpotlightPictures {
+function Copy-SpotlightPictures {
     Param(
         [Parameter(Position = 0)]
         [ValidateNotNullOrEmpty()]
-        [Alias("NewName")]
-        [String] $RenameAs = "SpotlightPhoto",
-        [switch] $OutputCopiedFiles
+        [String] $Name = "SpotlightPhoto"
     )
 
-    $SpotlightPhotosPath = Resolve-Path "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDelivery*\LocalState\Assets\"
-    if ($null -eq $SpotlightPhotosPath) {
-        Write-Error "Cannot resolve the Window's Spotlight directory path."
+    try {
+        $SpotlightPhotosPath = "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDelivery*\LocalState\Assets\*"
+        $Spotlights = Get-ChildItem $SpotlightPhotosPath
+    }
+    catch {
+        Write-Host "Cannot resolve the Window's Spotlight directory path."
         return
     }
 
-    $DestDirectory = New-Item "$env:USERPROFILE\Pictures\SpotlightPictures" -ItemType Directory -Force
-    $DateCreated = "{0:yyyyMMdd}" -f ([System.DateTime]::Now)
+    $DestDirectory = New-Item "$env:USERPROFILE\Pictures\SpotlightPhotos" -ItemType Directory -Force
+    $DateCreated = "{0:yyyyMMdd}" -f ([System.DateTime]::Today)
 
-    $ImageFiles = @(Get-ChildItem $SpotlightPhotosPath -File | 
-            ForEach-Object {
-                $Image = [System.Drawing.Image]::FromFile($_)
-                $IsJpg = $Image.RawFormat -eq [System.Drawing.Imaging.ImageFormat]::Jpeg
-                if ($IsJpg -and ($Image.Width -eq 1920)) {
-                    $_
-                }
-            })
-    $PassedFiles = 0
-    Push-Location $DestDirectory
-    for (($i = 0), ($j = 1); $i -lt $ImageFiles.Count; $i++) {
-        $destFileName = "{0}_{1}_{2}.jpg" -f $DateCreated, $RenameAs, $j
-        try {
-            $Jpg = Copy-Item $ImageFiles[$i] -Destination $destFileName -PassThru
-            $j++
-            $PassedFiles++
-            if ($OutputCopiedFiles) {
-                "Copy Successful: {0}" -f $Jpg | Write-Host
+    $FileIndex = 1
+    $TotalJpegs = 0
+    $StackName = "Spotlights"
+    Push-Location $DestDirectory -StackName $StackName
+    $ImageFiles = $Spotlights | ForEach-Object {
+        $Image = [System.Drawing.Image]::FromFile($_)
+        $IsJpg = $Image.RawFormat -eq [System.Drawing.Imaging.ImageFormat]::Jpeg
+        if ($IsJpg -and ($Image.Width -eq 1920)) {
+            $TotalJpegs++
+            $NewName = "{0}_{1}_{2}.jpg" -f $DateCreated, $Name, $FileIndex
+            try {
+                $Jpg = Copy-Item $_ $NewName -PassThru -ErrorAction Stop
+                $FileIndex++
             }
-        } catch {
-            Write-Warning $_.Exception.Message
+            catch {
+                Write-Error $_.Exception.Message
+                continue
+            }
+            "Copy Successful: {0}" -f $Jpg | Write-Verbose
+            $Jpg
         }
     }
-    Pop-Location
-    if ($ImageFiles.Count -ge 1) {
-        Write-Host ("{0}/{1} pictures copied successfully." -f $PassedFiles, $ImageFiles.Count)
+    Pop-Location -StackName $StackName
+    if ($TotalJpegs -ge 1) {
+        Write-Host ("{0}/{1} pictures copied successfully." -f $ImageFiles.Count, $TotalJpegs)
     }
     else {
         Write-Host "No files were found with specified criteria."
