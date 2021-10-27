e27cf3cb7f93efff7413190ea596ac5f4b61933b
marlfoxlegend <30681614+marlfoxlegend@users.noreply.github.com>
2021-10-27 17:14:13 -0400

    add function

--- MfxFunctions.ps1
+++ MfxFunctions.ps1
@@ -1,50 +1,63 @@
-﻿function Copy-SpotlightPictures {
+function Copy-SpotlightPictures {
     Param(
         [Parameter(Position = 0)]
         [ValidateNotNullOrEmpty()]
-        [String] $Name = "SpotlightPictures"
+        [String] $Name = "Spotlight",
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string] $Destination = 'Pictures\SpotlightPhotos'
     )
 
     try {
         $SpotlightDirectory = "$env:LOCALAPPDATA\Packages\Microsoft.Windows.ContentDelivery*\LocalState\Assets\*"
-        $Spotlights = Get-ChildItem -Path $SpotlightFiles
+        $Spotlights = @(Get-ChildItem -Path $SpotlightDirectory).FullName
     }
     catch {
-        Write-Error "Cannot resolve the Window's Spotlight directory path:`n`t$SpotlightDirectory"
+        Write-Host $_.Exception.Message
         return
     }
+    Write-Debug "Collected $($Spotlights.Count) files."
 
-    $DestDirectory = New-Item "$env:USERPROFILE\Pictures\SpotlightPictures" -ItemType Directory -Force
+    $Dest = if (!($Destination.Substring(0, 2) -ilike "?:")) {
+        "$env:USERPROFILE\$Destination"
+    }
+    else {
+        $Destination
+    }
+    $DestDirectory = New-Item $Dest -ItemType Directory -Force
     $DateCreated = "{0:yyyyMMdd}" -f ([System.DateTime]::Today)
 
     $FileIndex = 1
     $TotalJpegs = 0
-    $StackName = "Spotlights"
-    Push-Location $DestDirectory -StackName $StackName
-    $ImageFiles = $Spotlights | ForEach-Object {
-        $Image = [System.Drawing.Image]::FromFile($_)
+    $Spotlights | ForEach-Object {
+        try {
+            $Image = [System.Drawing.Image]::FromFile($_)
+            Write-Debug ("{0}; {1}" -f $Image.ToString(), $Image.PhysicalDimension)
+        }
+        catch {
+            Write-Warning $_.Exception.Message
+            continue
+        }
         $IsJpg = $Image.RawFormat -eq [System.Drawing.Imaging.ImageFormat]::Jpeg
         if ($IsJpg -and ($Image.Width -eq 1920)) {
             $TotalJpegs++
             $NewName = "{0}_{1}_{2}.jpg" -f $DateCreated, $Name, ("$FileIndex".PadLeft(2, '0'))
             try {
-                $Jpg = Copy-Item $_ $NewName -PassThru -ErrorAction Stop
+                $Jpg = Copy-Item $_ "$DestDirectory\$NewName" -PassThru
                 $FileIndex++
             }
             catch {
-                Write-Error -Exception $_.Exception
+                Write-Debug -Exception $_.Exception
                 continue
             }
-            Write-Information "Copy Successful: $Jpg"
-            $Jpg
-        }
+            Write-Information "Copied: $($_.Split('\')[-1])`nTo: $Jpg`n"
+        } 1> $null
     }
-    Pop-Location -StackName $StackName
     if ($TotalJpegs -ge 1) {
-        Write-Debug ("$($ImageFiles.Count)/$TotalJpegs pictures copied successfully.")
+        Write-Information ("$($FileIndex - 1)/$TotalJpegs pictures copied successfully.")
     }
     else {
-        Write-Debug "No files were found with specified criteria."
+        Write-Information "No files were found with specified criteria."
     }
 }
 
@@ -67,4 +80,23 @@ function wget {
     $name = $url.Substring($i);
     $destPath = Join-Path $destPath -ChildPath $name;
  (New-Object System.Net.WebClient).DownloadFile($url, $destPath);
+}
+
+Function gig {
+    param(
+        [Parameter(Mandatory, Position = 0)]
+        [string[]]$TemplateNames,
+        [Parameter()]
+        [switch]$PassThru
+    )
+    $Uri = "https://www.toptal.com/developers/gitignore/api/"
+    $Params = ($TemplateNames | ForEach-Object { [uri]::EscapeDataString($_) }) -join ","
+    $Content = Invoke-WebRequest -Uri ($Uri + $Params) | Select-Object -ExpandProperty content
+    if ($PassThru) {
+        Write-Output $Content
+    } else {
+        Out-File -InputObject $Content `
+            -FilePath (Join-Path -Path $PWD -ChildPath ".gitignore") `
+            -Encoding ascii
+    }
 }
